b904caffe6385329dfceb57293495a113d6b884a
Stephen Chung <arbstephen@gmail.com>
2014-12-01 22:27:09 -0500

    controller tweak

--- src/control/Controller.java
+++ src/control/Controller.java
@@ -215,6 +215,7 @@ public class Controller implements Serializable {
 			deque.addAll(eval(codeList.get(i), i));
 		}
 		canExecute = true;
+		System.out.println("There are " + deque.size() + " Executables.");
 	}
 	
 	/**
