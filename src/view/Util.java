08e3af8abef7ad98ac48d530f0d6dc5735a74024
it85 <isaac.tyan@gmail.com>
2014-12-11 01:30:01 -0500

    added images. see email about remaining changes

--- src/view/Util.java
+++ src/view/Util.java
@@ -1,14 +1,20 @@
 package view;
 
-import javax.swing.JLabel;
-import javax.swing.JList;
-import javax.swing.DefaultListModel;
-
+import java.awt.Color;
 import java.util.ArrayList;
 
-import model.*;
+import javax.swing.DefaultListModel;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JList;
+
+import model.BasicCode;
+import model.Code;
+import model.Code.Action;
+import model.Code.Proposition;
+import model.Karel;
+import model.World;
 import model.World.Contents;
-import model.Code.*;
 import control.Controller;
 
 public class Util {
@@ -141,39 +147,55 @@ public class Util {
 			for(int j = 0; j < world.getXSize(); j++) {
 				Contents contents = world.getContents(j, world.getYSize() - 1 - i);
 				JLabel label = worldLabels[i][j];
+				  worldLabels[i][j].setOpaque(true);
+				worldLabels[i][j].setBackground(Color.white);
+				
 				StringBuffer sb = new StringBuffer();
 				//sb.append(String.valueOf(j));
 				//sb.append(String.valueOf(world.getYSize() - 1 - i));
 				switch(contents) {
 				case BEEPER:
-					sb.append("Beeper ");
+					worldLabels[i][j].setIcon(new ImageIcon("images/beeper.png"));
+					//sb.append("Beeper ");
 					break;
 				case NONE:
-					//sb.append("N");
+					  worldLabels[i][j].setOpaque(true);
+					  worldLabels[i][j].setIcon(null);
+						worldLabels[i][j].setBackground(Color.white);
+
 					break;
 				case OUT_OF_BOUNDS:
-					sb.append("O ");
+					//sb.append("O ");
 					break;
 				case WALL:
-					sb.append("Wall ");
+					//sb.append("Wall ");
+			
+					worldLabels[i][j].setIcon(new ImageIcon("images/wall.png"));
+					            
 					break;
 				default:
 					break;
 				}
 				if(karel.getX() == j && karel.getY() == world.getYSize() - 1 - i) {
-					sb.append("Karel ");
+					ImageIcon image = new ImageIcon("images/right.png");
+					worldLabels[i][j].setIcon(image);
+					//sb.append("Karel ");
 					switch(karel.getFacing()) {
 					case EAST:
-						sb.append("/East");
+						worldLabels[i][j].setIcon(new ImageIcon("images/right.png"));
+
 						break;
 					case NORTH:
-						sb.append("/North");
+						worldLabels[i][j].setIcon(new ImageIcon("images/north.png"));
+
 						break;
 					case SOUTH:
-						sb.append("/South");
+						worldLabels[i][j].setIcon(new ImageIcon("images/south.png"));
+
 						break;
 					case WEST:
-						sb.append("/West");
+						worldLabels[i][j].setIcon(new ImageIcon("images/left.png"));
+
 						break;
 					default:
 						break;
