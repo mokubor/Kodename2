03d5a02dfe8e81b3216bc1134a8efbebe6525f5e
Stephen Chung <arbstephen@gmail.com>
2014-12-11 19:08:27 -0500

    Merge

--- src/view/MacroName.java
+++ src/view/MacroName.java
@@ -16,12 +16,8 @@ public class MacroName extends JPanel {
 	JLabel label = new JLabel("Macro Name:");
 	
 	public JTextField tx = new JTextField(25);
-<<<<<<< HEAD
 	public JButton ok;
-=======
-	public JButton ok = new JButton("Save");
->>>>>>> abba6035f58bf2ef572aa65172a346888be74a0e
-	
+
 	JPanel topPanel = new JPanel();
 	
 	public MacroName() {
